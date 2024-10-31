f7ae127b51240f8d05467b9e3eec83d5835c7b88
Shalom <shlomo000@gmail.com>
2024-10-31 09:50:39 +0200

    basic add task form added

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,6 +1,12 @@
 
 export const AddTask = () => {
   return (
-    <div>AddTask</div>
+    <div className='add-task'>
+      <h1>Military Opration Dashboard</h1>
+      <input type="text" placeholder='Name'/>
+      <input type="text" placeholder='Status'/>
+      <input type="text" placeholder='Priority'/>
+      <input type="text" placeholder='Description'/>
+    </div>
   )
 }
