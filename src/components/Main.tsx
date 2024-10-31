1ced28bee1f23519e47ece084e5fb7eda1cef206
Shalom <shlomo000@gmail.com>
2024-10-31 10:19:02 +0200

    add basic fetch cll and task state

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react'
 import { AddTask } from './AddTask'
 import { TaskList } from './TaskList'
+import Task from '../models/Task'
+
+const API_KEY = '9031537'
+const BASE_URL = `https://reactexambackend.onrender.com/missions/${API_KEY}`
 
 export const Main = () => {
+  const [tasks, setTasks] = useState<Task[]>()
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(BASE_URL)
+        setTasks(await res.json())
+        console.log(tasks)
+      } catch (err) {
+        console.log(err)
+        alert((err as Error).message)
+      }
+    })()
+  }, [])
+
   return (
     <div className='main'>
-      <AddTask/>
-      <TaskList/>
+      <AddTask />
+      <TaskList />
     </div>
   )
 }
