fd536c5e6268b19a85ea423fac8aa9c192bc802f
Shalom <shlomo000@gmail.com>
2024-10-31 11:38:37 +0200

    add task base comp

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,6 +1,18 @@
+import TaskModel from '../models/Task'
 
-export const Task = () => {
+interface Props {
+  task: TaskModel
+}
+
+export const Task = ({ task }: Props) => {
   return (
-    <div>Task</div>
+    <div className='task'>
+      <p>Name: {task.name}</p>
+      <p>Status: {task.status}</p>
+      <p>Priority: {task.priority}</p>
+      <p>Description: {task.description}</p>
+      <button>Progres</button>
+      <button>Delete task</button>
+    </div>
   )
 }
